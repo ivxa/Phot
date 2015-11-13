c6379b75bbdc4959e0752efbc01855beae74a1da
Xavier Paredes-Fortuny <xparedfo7@gmail.com>
2015-11-13 15:44:36 +0100

    Almost there...

---
+++ pipe_tjo.sh
@@ -0,0 +1,1 @@
+python main.py param_mwc656_tjo_B_ref_stars_presel.py && python main.py param_mwc656_tjo_B.py && python main.py param_mwc656_tjo_I.py && python main.py param_mwc656_tjo_R.py && python main.py param_mwc656_tjo_V.py && python colors.py
